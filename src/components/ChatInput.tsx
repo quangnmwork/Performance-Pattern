c351b4b38be76ece54282014f132269be240f428
quang.nm <quang.nm@teko.vn>
2023-02-23 14:38:44 +0700

    done dynamic import

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,8 +1,26 @@
+import React, { lazy, Suspense } from "react";
 import Send from "./icons/Send";
 import Emoji from "./icons/Emoji";
-import React from "react";
-import EmojiPicker from "./EmojiPicker";
+import loadable from "@loadable/component";
+// import EmojiPicker from "./EmojiPicker";
 
+// const Send = lazy(
+//   () => import(/*webpackChunkName: "send-icon" */ "./icons/Send")
+// );
+// const Emoji = lazy(
+//   () => import(/*webpackChunkName: "emoji-icon" */ "./icons/Emoji")
+// );
+
+// Lazy load EmojiPicker  when <EmojiPicker /> renders
+const Picker = lazy(
+  () => import(/*webpackChunkName: "emoji-picker" */ "./EmojiPicker")
+);
+
+// LazyLoad using loadable component
+
+const PickLoadable = loadable(() => import("./EmojiPicker"), {
+  fallback: <p id="loading">Load...</p>,
+});
 const ChatInput = () => {
   const [pickerOpen, togglePicker] = React.useReducer((state) => !state, false);
 
@@ -10,7 +28,9 @@ const ChatInput = () => {
     <div className="chat-input-container">
       <input type="text" placeholder="Type a message..." />
       <Emoji onClick={togglePicker} />
-      {pickerOpen && <EmojiPicker />}
+      <Suspense fallback={<p>Loading...</p>}>
+        {pickerOpen && <Picker />}
+      </Suspense>
       <Send />
     </div>
   );
